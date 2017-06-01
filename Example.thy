ea5b33c93da3705257b1b63947a59dae7d4b91e6
Hai Nguyen Van <hai.nguyen-van@lri.fr>
2017-06-01 02:14:44 +0200

    Pretty-printer for generated runs (issue #2)

--- Example.thy
+++ Example.thy
@@ -17,19 +17,11 @@ abbreviation \<Phi>\<^sub>0 where "\<Phi>\<^sub>0 \<equiv> [
 
 lemma "[], 0 \<Turnstile> [] \<triangleright> \<Phi>\<^sub>0"
 apply (heron_next_step)
-(*
-apply (rule instant_i, auto, solve_run_witness)
-  apply (rule sporadic_e2, solve_run_witness)
-  apply (rule sporadic_e1, solve_run_witness)
-  apply (rule implies_e2, solve_run_witness)
-*)
-apply (heron_next_step) print_facts
-(*
-apply (rule instant_i, auto, solve_run_witness)
-  apply (rule sporadic_e2, solve_run_witness)
-  apply (rule implies_e2, solve_run_witness)
-*)
+  ML_val\<open>print_run @{Isar.goal}\<close>
+apply (heron_next_step)
+  ML_val\<open>print_run @{Isar.goal}\<close>
+apply (heron_next_step)
+  ML_val\<open>print_run @{Isar.goal}\<close>
 by (heron_end)
-(* by (rule simulation_end, simp, solve_run_witness') *)
 
 end
--- Reiher.thy
+++ Reiher.thy
@@ -3,7 +3,138 @@ imports
   "Engine"
   "$ISABELLE_HOME/src/HOL/Eisbach/Eisbach_Tools" 
 
+keywords "print_run"::diag
+
 begin
+
+section\<open> Pretty-printer for runs \<close>
+
+(* Wanted outer syntax to use when pretty-printing runs *)
+ML \<open>
+val _ =
+  Outer_Syntax.command @{command_keyword print_run} "print run in the current Isar goal"
+    (Parse.opt_bang >> (fn b => Toplevel.keep (fn _ =>
+      let val x = Isar_Cmd.diag_goal (* ??? *)
+      in writeln "Not yet printing!" (* Pretty.writeln_chunks o Toplevel.pretty_context*) end)));
+\<close>
+
+(* Temporary solution *)
+ML\<open>
+local
+  datatype clock = Clk of string
+  type instant_index = int
+  datatype tag_const =
+      Unit
+    | Int of int
+  datatype tag_var =
+      Schematic of clock * instant_index
+  datatype tag_expr =
+      Constant of tag_const
+    | AddDelay of tag_var * tag_const
+  datatype constr =
+      Timestamp of clock * instant_index * tag_expr
+    | Ticks     of clock * instant_index
+    | NotTicks  of clock * instant_index
+    | Affine    of tag_var * tag_const * tag_var * tag_const
+in
+fun tag_const_of_term t = case t of
+    Const ("TESL.tag_const.Unit", _) => Unit
+  | Const ("TESL.tag_const.Integer", _) $ int_term => Int (snd (HOLogic.dest_number int_term))
+and tag_var_of_term t = case t of
+    Const ("TESL.tag_var.Schematic", _) $ (Const ("TESL.clock.Clk", _) $ clock_term) $ instant_index_term =>
+      Schematic (Clk (HOLogic.dest_string clock_term), HOLogic.dest_nat instant_index_term)
+exception DANGER
+fun constr_context_of_term gamma = case gamma of
+    Const ("List.list.Nil", _) => []
+  | Const ("List.list.Cons", _) $ constr_term $ gamma' => (case constr_term of
+        Const ("TESL.constr.Timestamp", _) $ (Const ("TESL.clock.Clk", _) $ clock_term) $ pos_term $ tag_expr_term =>
+          Timestamp (Clk (HOLogic.dest_string clock_term),
+                     HOLogic.dest_nat pos_term,
+                     case tag_expr_term of Const ("TESL.tag_expr.Const", _) $ tag_const_term =>
+                                             Constant (tag_const_of_term tag_const_term)
+                                         | Const ("TESL.tag_expr.AddDelay", _) $ tag_var_term $ tag_const_term =>
+                                             AddDelay (tag_var_of_term tag_var_term, tag_const_of_term tag_const_term))
+                                         
+      | Const ("TESL.constr.Ticks", _) $ (Const ("TESL.clock.Clk", _) $ clock_term) $ pos_term =>
+          Ticks (Clk (HOLogic.dest_string clock_term), HOLogic.dest_nat pos_term)
+      | Const ("TESL.constr.NotTicks", _) $ (Const ("TESL.clock.Clk", _) $ clock_term) $ pos_term =>
+          Ticks (Clk (HOLogic.dest_string clock_term), HOLogic.dest_nat pos_term)
+      | Const ("TESL.constr.Affine", _) $ tvar1 $ tconst1 $ tvar2 $ tconst2 =>
+          Affine (tag_var_of_term tvar1, tag_const_of_term tconst1, tag_var_of_term tvar2, tag_const_of_term tconst2)
+
+    ) :: constr_context_of_term gamma'
+
+fun string_of_tag (t : tag_const) =
+  case t of
+      Unit  => "()"
+    | Int n => string_of_int n
+
+fun print_system (step_index: int) (clocks: clock list) (G : constr list) =
+  let
+    (* Old tick symbol: \226\135\145 to get \<Up> *)
+    val TICK_SYM = "\226\134\145"       (* \<up> *)
+    val FORBIDDEN_SYM = "\226\138\152"  (* \<oslash> *)
+    fun constrs_of_clk_instindex c n =
+      List.filter (fn Ticks (c', n') => c = c' andalso n = n' | NotTicks (c', n') => c = c' andalso n = n' | Timestamp (c', n', _) => c = c' andalso n = n' | _ => false) G
+    fun string_of_constrs_at_clk_instindex clk n g =
+      let
+        val timestamps = List.filter (fn Timestamp (_, _, Constant tag) => (case tag of Unit => true | Int _ => true | _ => false) | _ => false) g
+	 val tick_string =
+	   if List.exists (fn x => x = Ticks (clk, n)) g
+	   then TICK_SYM
+	   else
+	     if List.exists (fn x => x = NotTicks (clk, n)) g
+	     then FORBIDDEN_SYM
+	     else ""
+	 val tag_string =
+	     case List.find (fn Timestamp (_, _, _) => true | _ => false) timestamps of
+		  NONE                         => " "
+		| SOME (Timestamp (_, _, Constant tag)) => string_of_tag tag
+      in tick_string ^ " " ^ tag_string
+    end
+    fun print_clocks () =
+      writeln ("\t\t" ^ List.foldr (fn (Clk c, s) => c ^ "\t\t" ^ s) "" clocks)
+    fun print_instant n =
+      writeln ("[" ^ string_of_int n ^ "]"
+		 ^ List.foldl (fn (c, s) => s ^ "\t\t" ^ string_of_constrs_at_clk_instindex c n (constrs_of_clk_instindex c n)) "" clocks)
+    fun print_run k =
+      if k > step_index
+      then ()
+      else (print_instant k ; print_run (k + 1))
+  in print_clocks (); print_run 1
+end
+
+fun uniq l =
+  let fun aux (x :: l') acc =
+          if List.exists (fn x' => x' = x) acc
+          then aux l' acc
+          else aux l' (x :: acc)
+      | aux [] acc             = acc
+  in List.rev (aux l [])
+  end
+and clocks_of_system (G: constr list) =
+  uniq (List.concat (List.map (fn
+    Timestamp (c, _, _) => [c]
+  | Ticks (c, _)        => [c]
+  | NotTicks (c, _)     => [c]
+  | _ => []
+  ) G))
+
+fun print_run isar_state = let
+  val {context, facts, goal} = isar_state (* @{Isar.goal} *)
+  val imp_cst $ (true_prop $ (truc $ gamma $ run_index $ psi $ phi)) $ _ = Thm.prop_of goal
+  val parsed_constraints = constr_context_of_term gamma
+  in
+    writeln "Run diagram:";
+    print_system (HOLogic.dest_nat run_index) (clocks_of_system parsed_constraints) parsed_constraints
+  end
+
+end
+
+(* val use_example = print_run @{Isar.goal}; *)
+\<close>
+
+section\<open> Main solver at a glance \<close>
 
 method solve_run_witness =
   rule witness_consistency,
@@ -28,5 +159,8 @@ method heron_next_step =
 
 method heron_end =
   rule simulation_end, simp, solve_run_witness_end
+
+(* Are Eisbach methods recursive? *)
+method heron_step for n ::nat = (heron_step \<open>Suc n\<close>)
 
 end
