3ec2ae77e2aa284e041298cb5ed4c00f8b5b00b5
Hai Nguyen Van <hai.nguyen-van@lri.fr>
2017-06-06 23:56:39 +0200

    Fixed executability by temporarily `admitting' monotonicity. Minor changes in syntax abbrevs.

--- Reiher_Example.thy
+++ Reiher_Example.thy
@@ -15,10 +15,84 @@ abbreviation \<Phi>\<^sub>0 where "\<Phi>\<^sub>0 \<equiv> [
     \<H>\<^sub>1 implies \<H>\<^sub>2
 ]"
 
+lemma my_abs_inv: "\<forall>c. mono (\<lambda>n. time (\<rho> n c)) \<Longrightarrow> Rep_run (Abs_run \<rho>) = \<rho>"
+  by (simp add: Abs_run_inverse)
+lemma my_abs_inv'[simp]: "Rep_run (Abs_run \<rho>) = \<rho>"
+  (* TEMPORARY SOLUTION OF THE PROBLEM *)
+  sorry
+
+
 lemma "[], 0 \<Turnstile> [] \<triangleright> \<Phi>\<^sub>0"
+apply (heron_next_step)
+(*
+apply (rule instant_i, auto, solve_run_witness)
+  apply (rule sporadic_e2)
+
+  apply (rule witness_consistency)
+  apply (auto)
+
+
+  apply (rule sporadic_e1, solve_run_witness)
+  apply (rule implies_e2, solve_run_witness)
+*)
 apply (heron_next_step) print_run
-apply (heron_next_step) print_run
-apply (heron_next_step) print_run
+(*
+apply (rule instant_i, auto, solve_run_witness)
+  apply (rule sporadic_e2, solve_run_witness)
+  apply (rule implies_e2, solve_run_witness)
+*)
 by (heron_end)
+(* by (rule simulation_end, simp, solve_run_witness') *)
+
+(* Some draft proofs to handle [undefined] constant with monotonicity properties *)
+lemma hamlet_is_fst: "hamlet (x, y) = x" by simp
+lemma time_is_snd: "time (x, y) = y" by simp
+lemma hamlet_ite: "hamlet (if P then (x1, x2) else (y1, y2)) = (if P then x1 else y1)" by simp
+lemma time_ite: "time (if P then (x1, x2) else (y1, y2)) = (if P then x2 else y2)" by simp
+
+lemma "hamlet
+     (Rep_run
+       (Abs_run
+         (\<lambda>n' K'.
+             if \<H>\<^sub>1 = K' \<and> Suc 0 = n'
+             then (True,
+                   time
+                    (Rep_run
+                      (Abs_run
+                        (\<lambda>n' K'.
+                            if \<H>\<^sub>1 = K' \<and> Suc 0 = n'
+                            then (hamlet (Rep_run \<rho>\<^sub>\<odot> (Suc 0) \<H>\<^sub>1),
+                                  \<tau>\<^sub>i\<^sub>n\<^sub>t 1)
+                            else Rep_run \<rho>\<^sub>\<odot> n' K'))
+                      (Suc 0) \<H>\<^sub>1))
+             else Rep_run
+                   (Abs_run
+                     (\<lambda>n' K'.
+                         if \<H>\<^sub>1 = K' \<and> Suc 0 = n'
+                         then (hamlet (Rep_run \<rho>\<^sub>\<odot> (Suc 0) \<H>\<^sub>1), \<tau>\<^sub>i\<^sub>n\<^sub>t 1)
+                         else Rep_run \<rho>\<^sub>\<odot> n' K'))
+                   n' K'))
+       (Suc 0) \<H>\<^sub>1)"
+apply ((subst my_abs_inv)[])+ apply auto+
+apply (simp add: mono_def)
+apply ((subst my_abs_inv)[])+ apply auto+
+apply (simp add: mono_def)
+apply (subst hamlet_is_fst)+
+apply (subst time_ite)
+apply (simp add: mono_def) apply auto+
+
+prefer 3
+apply ((subst my_abs_inv)[])+ apply auto+
+apply (simp add: mono_def)
+apply (subst hamlet_is_fst)+
+apply ((subst my_abs_inv)[])+ apply auto+
+apply (simp add: mono_def)
+apply (subst hamlet_is_fst)+ 
+
+prefer 2
+apply ((subst my_abs_inv)[])+ apply auto+
+apply (simp add: mono_def)
+apply (subst hamlet_is_fst time_is_snd)+
+sorry
 
 end
--- Run.thy
+++ Run.thy
@@ -17,15 +17,17 @@ typedef (overloaded) run =
   "{ \<rho>::nat \<Rightarrow> clock \<Rightarrow> (bool \<times> tag_const). \<forall>c. mono (\<lambda>n. time (\<rho> n c)) }"
 sorry
 
+(*
 abbreviation Abs_run_not ("_\<^sup>\<up>" 1000) where "f\<^sup>\<up> \<equiv> Abs_run f"
 abbreviation Rep_run_not ("_\<^sup>\<down>" 1000) where "f\<^sup>\<down> \<equiv> Rep_run f"
+*)
 
 fun symbolic_run_interpretation_primitive :: "constr \<Rightarrow> run set" ("\<lbrakk> _ \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n") where
-    "\<lbrakk> K \<Up> n  \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. hamlet (\<rho>\<^sup>\<down> n K) = True }"
-  | "\<lbrakk> K \<not>\<Up> n \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. hamlet (\<rho>\<^sup>\<down> n K) = False }"
-  | "\<lbrakk> K \<Down> n @ \<lfloor> \<tau> \<rfloor> \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. time (\<rho>\<^sup>\<down> n K) = \<tau> }"
-  | "\<lbrakk> K \<Down> n @ \<lfloor> \<tau>\<^sub>v\<^sub>a\<^sub>r(K', n') \<oplus> \<tau> \<rfloor> \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. time (\<rho>\<^sup>\<down> n K) = time (\<rho>\<^sup>\<down> n' K') + \<tau> }"
-  | "\<lbrakk> \<tau>\<^sub>v\<^sub>a\<^sub>r(K\<^sub>1, n\<^sub>1) \<doteq> \<alpha> * \<tau>\<^sub>v\<^sub>a\<^sub>r(K\<^sub>2, n\<^sub>2) + \<beta> \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. time (\<rho>\<^sup>\<down> n\<^sub>1 K\<^sub>1) = \<alpha> * time (\<rho>\<^sup>\<down> n\<^sub>2 K\<^sub>2) + \<beta> }"
+    "\<lbrakk> K \<Up> n  \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. hamlet ((Rep_run \<rho>) n K) = True }"
+  | "\<lbrakk> K \<not>\<Up> n \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. hamlet ((Rep_run \<rho>) n K) = False }"
+  | "\<lbrakk> K \<Down> n @ \<lfloor> \<tau> \<rfloor> \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. time ((Rep_run \<rho>) n K) = \<tau> }"
+  | "\<lbrakk> K \<Down> n @ \<lfloor> \<tau>\<^sub>v\<^sub>a\<^sub>r(K', n') \<oplus> \<tau> \<rfloor> \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. time ((Rep_run \<rho>) n K) = time ((Rep_run \<rho>) n' K') + \<tau> }"
+  | "\<lbrakk> \<tau>\<^sub>v\<^sub>a\<^sub>r(K\<^sub>1, n\<^sub>1) \<doteq> \<alpha> * \<tau>\<^sub>v\<^sub>a\<^sub>r(K\<^sub>2, n\<^sub>2) + \<beta> \<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { \<rho>. time ((Rep_run \<rho>) n\<^sub>1 K\<^sub>1) = \<alpha> * time ((Rep_run \<rho>) n\<^sub>2 K\<^sub>2) + \<beta> }"
 
 fun symbolic_run_interpretation :: "constr list \<Rightarrow> run set" ("\<lbrakk>\<lbrakk> _ \<rbrakk>\<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n") where
     "\<lbrakk>\<lbrakk> [] \<rbrakk>\<rbrakk>\<^sub>s\<^sub>y\<^sub>m\<^sub>r\<^sub>u\<^sub>n = { _. True }"
@@ -42,18 +44,18 @@ abbreviation initial_run :: "run" ("\<rho>\<^sub>\<odot>") where
 
 (* Update functionals *)
 fun run_update :: "run \<Rightarrow> constr \<Rightarrow> run" ("_ \<langle> _ \<rangle>") where
-    "\<rho> \<langle> K \<Up> n \<rangle>  = (\<lambda>n' K'. if K = K' \<and> n = n' then (True, time (\<rho>\<^sup>\<down> n K)) else \<rho>\<^sup>\<down> n' K')\<^sup>\<up>"
-  | "\<rho> \<langle> K \<not>\<Up> n \<rangle> = (\<lambda>n' K'. if K = K' \<and> n = n' then (False, time (\<rho>\<^sup>\<down> n K)) else \<rho>\<^sup>\<down> n' K')\<^sup>\<up>"
-  | "\<rho> \<langle> K \<Down> n @ \<lfloor> \<tau> \<rfloor> \<rangle> = (\<lambda>n' K'. if K = K' \<and> n = n' then (hamlet (\<rho>\<^sup>\<down> n K), \<tau>) else \<rho>\<^sup>\<down> n' K')\<^sup>\<up>"
+    "\<rho> \<langle> K \<Up> n \<rangle>  = Abs_run (\<lambda>n' K'. if K = K' \<and> n = n' then (True, time ((Rep_run \<rho>) n K)) else (Rep_run \<rho>) n' K')"
+  | "\<rho> \<langle> K \<not>\<Up> n \<rangle> = Abs_run (\<lambda>n' K'. if K = K' \<and> n = n' then (False, time ((Rep_run \<rho>) n K)) else (Rep_run \<rho>) n' K')"
+  | "\<rho> \<langle> K \<Down> n @ \<lfloor> \<tau> \<rfloor> \<rangle> = Abs_run (\<lambda>n' K'. if K = K' \<and> n = n' then (hamlet ((Rep_run \<rho>) n K), \<tau>) else (Rep_run \<rho>) n' K')"
   | "\<rho> \<langle> K \<Down> n @ \<lfloor> \<tau>\<^sub>v\<^sub>a\<^sub>r(K', n') \<oplus> \<tau> \<rfloor> \<rangle> =
-     (\<lambda>n'' K''. if K = K'' \<and> n = n'' then (hamlet (\<rho>\<^sup>\<down> n K), time (\<rho>\<^sup>\<down> n' K') + \<tau>) else \<rho>\<^sup>\<down> n'' K'')\<^sup>\<up>"
+     Abs_run (\<lambda>n'' K''. if K = K'' \<and> n = n'' then (hamlet ((Rep_run \<rho>) n K), time ((Rep_run \<rho>) n' K') + \<tau>) else (Rep_run \<rho>) n'' K'')"
   | "\<rho> \<langle> \<tau>\<^sub>v\<^sub>a\<^sub>r(K\<^sub>1, n\<^sub>1) \<doteq> \<alpha> * \<tau>\<^sub>v\<^sub>a\<^sub>r(K\<^sub>2, n\<^sub>2) + \<beta> \<rangle> =
-     (if time (\<rho>\<^sup>\<down> n\<^sub>1 K\<^sub>1) = undefined \<and> time (\<rho>\<^sup>\<down> n\<^sub>2 K\<^sub>2) \<noteq> undefined
-      then (\<lambda>n K. if K = K\<^sub>1 \<and> n = n\<^sub>1 then (hamlet (\<rho>\<^sup>\<down> n K), \<alpha> * time (\<rho>\<^sup>\<down> n\<^sub>2 K\<^sub>2) + \<beta>) else \<rho>\<^sup>\<down> n K)
-      else if time (\<rho>\<^sup>\<down> n\<^sub>2 K\<^sub>2) = undefined \<and> time (\<rho>\<^sup>\<down> n\<^sub>1 K\<^sub>1) \<noteq> undefined
-           then (\<lambda>n K. if K = K\<^sub>2 \<and> n = n\<^sub>2 then (hamlet (\<rho>\<^sup>\<down> n K), undefined (* (time (\<rho> n\<^sub>1 K\<^sub>1) - \<beta>) / \<alpha> *)) else \<rho>\<^sup>\<down> n K)
-           else \<rho>\<^sup>\<down> (* MAYBE NOT A GOOD CHOICE *)
-     )\<^sup>\<up>"
+     Abs_run (if time ((Rep_run \<rho>) n\<^sub>1 K\<^sub>1) = undefined \<and> time ((Rep_run \<rho>) n\<^sub>2 K\<^sub>2) \<noteq> undefined
+      then (\<lambda>n K. if K = K\<^sub>1 \<and> n = n\<^sub>1 then (hamlet ((Rep_run \<rho>) n K), \<alpha> * time ((Rep_run \<rho>) n\<^sub>2 K\<^sub>2) + \<beta>) else (Rep_run \<rho>) n K)
+      else if time ((Rep_run \<rho>) n\<^sub>2 K\<^sub>2) = undefined \<and> time ((Rep_run \<rho>) n\<^sub>1 K\<^sub>1) \<noteq> undefined
+           then (\<lambda>n K. if K = K\<^sub>2 \<and> n = n\<^sub>2 then (hamlet ((Rep_run \<rho>) n K), undefined (* (time (\<rho> n\<^sub>1 K\<^sub>1) - \<beta>) / \<alpha> *)) else (Rep_run \<rho>) n K)
+           else (Rep_run \<rho>) (* MAYBE NOT A GOOD CHOICE *)
+     )"
 
 fun run_update' :: "constr list \<Rightarrow> run" ("\<langle>\<langle> _ \<rangle>\<rangle>") where
     "\<langle>\<langle> [] \<rangle>\<rangle>    = \<rho>\<^sub>\<odot>"
