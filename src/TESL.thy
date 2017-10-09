62c9daf9e2efe361665716364e0fa7fea958927c
Hai Nguyen Van <hai.nguyen-van@lri.fr>
2017-10-09 23:49:53 +0200

    Attempts to generalize types

--- src/TESL.thy
+++ src/TESL.thy
@@ -5,50 +5,50 @@ begin
 text {* We define as follows the syntax of primitives to describe symbolic runs *}
 
 (** Clocks **)
-datatype clock = Clk "string"        ("\<lceil> _ \<rceil>")
+datatype '\<kappa> clock = Clk "'\<kappa>"        ("\<lceil> _ \<rceil>")
 type_synonym instant_index = "nat"
 
 (** Tags **) 
 (* Constants *)
-datatype tag_const =
-    Rational   "rat"                   ("\<tau>\<^sub>r\<^sub>a\<^sub>t")
+datatype '\<tau> tag_const =
+    Rational   "'\<tau>"                   ("\<tau>\<^sub>r\<^sub>a\<^sub>t")
 (* Variables *)
-datatype tag_var =
-    Schematic "clock * instant_index" ("\<tau>\<^sub>v\<^sub>a\<^sub>r")
+datatype '\<kappa> tag_var =
+    Schematic "'\<kappa> clock * instant_index" ("\<tau>\<^sub>v\<^sub>a\<^sub>r")
 (* Expressions *)
-datatype tag_expr =
-    Const    "tag_const"           ("\<lfloor> _ \<rfloor>")
-  | AddDelay "tag_var" "tag_const" ("\<lfloor> _ \<oplus> _ \<rfloor>")
+datatype ('\<kappa>, '\<tau>) tag_expr =
+    Const    "'\<tau> tag_const"           ("\<lfloor> _ \<rfloor>")
+  | AddDelay "'\<kappa> tag_var" "'\<tau> tag_const" ("\<lfloor> _ \<oplus> _ \<rfloor>")
 
 (* Primitives for symbolic runs *)
-datatype constr =
-    Timestamp "clock"   "instant_index" "tag_expr"            ("_ \<Down> _ @ _")
-  | Ticks     "clock"   "instant_index"                       ("_ \<Up> _")
-  | NotTicks  "clock"   "instant_index"                       ("_ \<not>\<Up> _")
-  | Affine    "tag_var" "tag_const"     "tag_var" "tag_const" ("_ \<doteq> _ * _ + _")
-  | ArithGen  "tag_var" "tag_var" "(tag_const \<times> tag_const) \<Rightarrow> bool" ("\<langle>_, _\<rangle> \<epsilon> _")
+datatype ('\<kappa>, '\<tau>) constr =
+    Timestamp "'\<kappa> clock"   "instant_index" "('\<kappa>, '\<tau>) tag_expr"            ("_ \<Down> _ @ _")
+  | Ticks     "'\<kappa> clock"   "instant_index"                       ("_ \<Up> _")
+  | NotTicks  "'\<kappa> clock"   "instant_index"                       ("_ \<not>\<Up> _")
+  | Affine    "'\<kappa> tag_var" "'\<tau> tag_const"     "'\<kappa> tag_var" "'\<tau> tag_const" ("_ \<doteq> _ * _ + _")
+  | ArithGen  "'\<kappa> tag_var" "'\<kappa> tag_var" "('\<tau> tag_const \<times> '\<tau> tag_const) \<Rightarrow> bool" ("\<langle>_, _\<rangle> \<epsilon> _")
 
-type_synonym system = "constr list"
+type_synonym ('\<kappa>, '\<tau>) system = "('\<kappa>, '\<tau>) constr list"
 
 text{* Define as follows the syntax of TESL *}
 
 (* TESL language *)
-datatype TESL_atomic =
-    Sporadic       "clock" "tag_const"                     (infixr "sporadic" 55)
-  | SporadicOn     "clock" "tag_expr"  "clock"             ("_ sporadic _ on _" 55)
-  | TagRelation    "clock" "tag_const" "clock" "tag_const" ("tag-relation _ = _ * _ + _" 55)
-  | TagRelationGen "clock" "clock" "(tag_const \<times> tag_const) \<Rightarrow> bool" ("tag-relation \<langle>_, _\<rangle> \<in> _" 55)
-  | Implies        "clock" "clock"                         (infixr "implies" 55)
-  | TimeDelayedBy  "clock" "tag_const" "clock" "clock"     ("_ time-delayed by _ on _ implies _" 55)
+datatype ('\<kappa>, '\<tau>) TESL_atomic =
+    Sporadic       "'\<kappa> clock" "'\<tau> tag_const"                     (infixr "sporadic" 55)
+  | SporadicOn     "'\<kappa> clock" "('\<kappa>, '\<tau>) tag_expr"  "'\<kappa> clock"             ("_ sporadic _ on _" 55)
+  | TagRelation    "'\<kappa> clock" "'\<tau> tag_const" "'\<kappa> clock" "'\<tau> tag_const" ("tag-relation _ = _ * _ + _" 55)
+  | TagRelationGen "'\<kappa> clock" "'\<kappa> clock" "('\<tau> tag_const \<times> '\<tau> tag_const) \<Rightarrow> bool" ("tag-relation \<langle>_, _\<rangle> \<in> _" 55)
+  | Implies        "'\<kappa> clock" "'\<kappa> clock"                         (infixr "implies" 55)
+  | TimeDelayedBy  "'\<kappa> clock" "'\<tau> tag_const" "'\<kappa> clock" "'\<kappa> clock"     ("_ time-delayed by _ on _ implies _" 55)
 
-type_synonym TESL_formula = "TESL_atomic list"
+type_synonym ('\<kappa>, '\<tau>) TESL_formula = "('\<kappa>, '\<tau>) TESL_atomic list"
 
-fun positive_atom :: "TESL_atomic \<Rightarrow> bool" where
+fun positive_atom :: "('\<kappa>, '\<tau>) TESL_atomic \<Rightarrow> bool" where
     "positive_atom (_ sporadic _)      = True"
   | "positive_atom (_ sporadic _ on _) = True"
   | "positive_atom _                   = False"
 
-abbreviation NoSporadic :: "TESL_formula \<Rightarrow> TESL_formula" where 
+abbreviation NoSporadic :: "('\<kappa>, '\<tau>) TESL_formula \<Rightarrow> ('\<kappa>, '\<tau>) TESL_formula" where 
   "NoSporadic f \<equiv> (List.filter (\<lambda>f\<^sub>a\<^sub>t\<^sub>o\<^sub>m. case f\<^sub>a\<^sub>t\<^sub>o\<^sub>m of
       _ sporadic _  \<Rightarrow> False
     | _ sporadic _ on _ \<Rightarrow> False
@@ -58,7 +58,7 @@ abbreviation NoSporadic :: "TESL_formula \<Rightarrow> TESL_formula" where
    Follows the intuition: past [\<Gamma>], current index [n], present [\<psi>], future [\<phi>]
    Beware: This type is slightly different from which originally implemented in Heron
 *)
-type_synonym config = "system * instant_index * TESL_formula * TESL_formula"
+type_synonym ('\<kappa>, '\<tau>) config = "('\<kappa>, '\<tau>) system * instant_index * ('\<kappa>, '\<tau>) TESL_formula * ('\<kappa>, '\<tau>) TESL_formula"
 
 (*declare[[show_sorts]]*)
 
